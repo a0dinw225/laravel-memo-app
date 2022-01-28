06a6c2c91808de1929ee4f3d9543485e7c9bd760
Hiro Anzai <hiro@MacBook-Pro.local>
2022-01-29 08:57:25 +0900

    Common memo list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,16 +27,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'DESC')
-            ->get();
-
         $tags = Tag::where('user_id', '=', \Auth::id())->where('deleted_at')->orderBy('id', 'DESC')
         ->get();
 
-        return view('create', compact('memos', 'tags'));
+        return view('create', compact('tags'));
     }
 
     public function store(Request $request)
@@ -71,12 +65,6 @@ class HomeController extends Controller
 
     public function edit($id)
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'DESC')
-            ->get();
-
         $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
         ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
         ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
@@ -92,7 +80,7 @@ class HomeController extends Controller
         $tags = Tag::where('user_id', '=', \Auth::id())->where('deleted_at')->orderBy('id', 'DESC')
         ->get();
 
-        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
+        return view('edit', compact('edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
