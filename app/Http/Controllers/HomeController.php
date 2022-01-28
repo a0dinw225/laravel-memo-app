6f045fd8ae0e47f3a8867bcef311ef947ebddfc3
Hiro Anzai <hiro@MacBook-Pro.local>
2022-01-28 19:26:50 +0900

    update memo index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('create');
+        $memos = Memo::select('memos.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC')
+            ->get();
+
+        return view('create', compact('memos'));
+    }
+
+    public function store(Request $request)
+    {
+        $posts = $request->all();
+        Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
+        return redirect( route('home') );
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,9 +87,11 @@
                 </div>
                 <div class="col-md-4 p-0">
                     <div class="card">
-                        <div class="card-header">中央カラム</div>
+                        <div class="card-header">メモ一覧</div>
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    @foreach($memos as $memo)
+                            <a class="card-text d-block">{{ $memo['content'] }}</a>
+                    @endforeach
                         </div>
                     </div>
                 </div>
