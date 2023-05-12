fe196acb2c581c7274ee3faa84d6954fb2869df2
root <hiro@MacBook-Pro.local>
2023-05-12 14:00:43 +0000

    create MemoTagRepositoryTest:it_can_get_user_memo_with_tag

--- tests/Unit/MemoTagRepositoryTest.php
+++ tests/Unit/MemoTagRepositoryTest.php
@@ -29,6 +29,26 @@ class MemoTagRepositoryTest extends TestCase
     }
 
     /** @test */
+    public function it_can_get_user_memo_with_tag(): void
+    {
+        $user = User::factory()->create();
+        $memo = Memo::factory()->create(['user_id' => $user->id]);
+        $tag = Tag::factory()->create(['user_id' => $user->id]);
+        MemoTag::factory()->create([
+            'user_id' => $user->id,
+            'memo_id' => $memo->id,
+            'tag_id' => $tag->id,
+        ]);
+
+        $memoTags = $this->memoTagRepository->getUserMemoWithTag($user->id, $memo->id);
+
+        $this->assertCount(1, $memoTags);
+        $this->assertSame($user->id, $memoTags[0]['user_id']);
+        $this->assertSame($memo->id, $memoTags[0]['memo_id']);
+        $this->assertSame($tag->id, $memoTags[0]['tag_id']);
+    }
+
+    /** @test */
     public function it_can_insert_memo_tag(): void
     {
         $user = User::factory()->create();
