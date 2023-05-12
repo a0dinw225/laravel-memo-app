6145f4971ed5d4e8c4be1d15562be0a57426bd1f
a0dinw225 <hiro@MacBook-Pro.local>
2023-05-12 23:10:53 +0900

    create MemoTagServiceTest:it_can_get_user_memo_with_tag

--- tests/Unit/MemoTagServiceTest.php
+++ tests/Unit/MemoTagServiceTest.php
@@ -31,6 +31,26 @@ class MemoTagServiceTest extends TestCase
     }
 
     /** @test */
+    public function it_can_get_user_memo_with_tag(): void
+    {
+        $user = User::factory()->create();
+        $memo = Memo::factory()->create(['user_id' => $user->id]);
+        $tag = Tag::factory()->create(['user_id' => $user->id]);
+        MemoTag::factory()->create([
+            'user_id' => $user->id,
+            'memo_id' => $memo->id,
+            'tag_id' => $tag->id,
+        ]);
+
+        $memoTags = $this->memoTagService->getUserMemoWithTag($user->id, $memo->id);
+
+        $this->assertCount(1, $memoTags);
+        $this->assertSame($user->id, $memoTags[0]['user_id']);
+        $this->assertSame($memo->id, $memoTags[0]['memo_id']);
+        $this->assertSame($tag->id, $memoTags[0]['tag_id']);
+    }
+
+    /** @test */
     public function it_can_delete_memo_tag(): void
     {
         $user = User::factory()->create();
